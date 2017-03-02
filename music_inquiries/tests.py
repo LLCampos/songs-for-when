bbeb7cab3b5ec747a1191374dbc50a9c2282850a
LLCampos <luis1212@sapo.pt>
2017-03-02 09:45:17 +0000

    add tests

--- music_inquiries/tests.py
+++ music_inquiries/tests.py
@@ -1,3 +1,39 @@
 from django.test import TestCase
+from music_inquiries.models import Song
 
-# Create your tests here.
+# Test Models
+
+
+class TestSongModel(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_create_song(self):
+
+        song = Song.objects.create_song(
+            artist_name='Rihanna',
+            song_name='Umbrella',
+            youtube_url='https://www.youtube.com/watch?v=CvBfHwUxHIk'
+        )
+
+        self.assertEqual(song.song_name, 'Umbrella')
+        self.assertEqual(song.artist_name, 'Rihanna')
+
+    def test_does_song_exist(self):
+
+        Song.objects.create_song(
+            artist_name='Metallica',
+            song_name='Enter Sandman',
+            youtube_url='https://www.youtube.com/watch?v=CvBfHwUxHIk'
+        )
+
+        self.assertTrue(Song.objects.does_song_exist(
+            artist_name='Metallica',
+            song_name='Enter Sandman')
+        )
+
+        self.assertFalse(Song.objects.does_song_exist(
+            artist_name='Megadeth',
+            song_name='A Garagem da Vizinha')
+        )
