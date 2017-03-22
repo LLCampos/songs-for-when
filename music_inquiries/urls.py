ef48dffb332a2ccab7b71705954b9a5eafec2f1b
LLCampos <luis1212@sapo.pt>
2017-03-22 20:51:08 +0000

    cosmetic changes to code

--- music_inquiries/urls.py
+++ music_inquiries/urls.py
@@ -4,11 +4,14 @@ from . import views
 
 app_name = 'music_inquiries'
 urlpatterns = [
+    # HTML Pages
     url(r'^$', views.index, name='index'),
     url(r'^inquiry/$', views.inquiries_listing, name='inquiries_listing'),
+    url(r'^inquiry/(?P<inquiry_id>[0-9]+)/$', views.inquiry, name='inquiry'),
+
+    # REST Internal API
     url(r'^inquiry/(?P<inquiry_id>[0-9]+)/suggestion', views.suggestion, name='suggestion'),
     url(r'^inquiry/(?P<inquiry_id>[0-9]+)/inquiry_report', views.inquiry_report, name='inquiry_report'),
-    url(r'^inquiry/(?P<inquiry_id>[0-9]+)/$', views.inquiry, name='inquiry'),
     url(r'^inquiry/search/$', views.inquiry_search, name='inquiry_search'),
     url(r'^song/$', views.song, name='song'),
 ]
