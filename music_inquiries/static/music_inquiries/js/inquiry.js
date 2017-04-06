34a18ba1184b4087cf20b6794ebec4ab4fef5db2
LLCampos <luis1212@sapo.pt>
2017-04-06 19:07:18 +0100

    work on suggestion form modal

--- music_inquiries/static/music_inquiries/js/inquiry.js
+++ music_inquiries/static/music_inquiries/js/inquiry.js
@@ -1,5 +1,45 @@
+// Returns true if both input values (for song artist and song name) are not empty
+const suggestionArtistAndSongInputFilled = () => {
+  const artistValue = $('#suggestion-input-artist').val().trim();
+  const songValue = $('#suggestion-input-song').val().trim();
+
+  if (artistValue !== '' && songValue !== '') {
+    return true;
+  }
+  return false;
+};
+
+
 $(() => {
   $('.suggestions-carousel-control').click(() => {
     pauseYoutubeVideos();
+  });
+
+  $('#suggestion-input-artist, #suggestion-input-song').keyup(() => {
+    const searchButton = $('#suggestion-input-search');
+    if (searchButton.prop('disabled') && suggestionArtistAndSongInputFilled()) {
+      searchButton.prop('disabled', false);
+    } else if (!searchButton.prop('disabled') && !suggestionArtistAndSongInputFilled()) {
+      searchButton.prop('disabled', true);
+    }
+  });
+
+  $('#suggestion-input-search').click(() => {
+    const artistValue = $('#suggestion-input-artist').val().trim();
+    const songValue = $('#suggestion-input-song').val().trim();
+
+    $.ajax('/iapi/song', {
+      type: 'HEAD',
+      data: {
+        artist_name: artistValue,
+        song_name: songValue,
+      },
+      success: () => {
+        $('#suggestion-form-submit').prop('disabled', false);
+      },
+      error: () => {
+        console.log('not exist');
+      },
+    });
   });
 });
